319358e637546c3999be4549494591c1845f9877
abdialimovtemur <abdialimovtemur@gmail.com>
2024-08-30 11:47:50 +0500

    Commit title

--- main.js
+++ main.js
@@ -18,9 +18,18 @@ const render = () => {
         // div.append(img,info,p); 
         // cards.appendChild(div)
     // }
-    data.map(item =>{
-        cards.innerHTML = ` 
-        <div></div>`
+    console.log(data);
+    
+    data.forEach(item =>{
+        cards.innerHTML += ` 
+        <div class="flex bg-[#F6F6F6] rounded-[20px] p-2 gap-2 items-center ">
+        <div class="min-h-full w-4/12"><img class="min-h-full object-cover" src="${item.img}" alt=""></div>
+        <div class=" flex flex-col gap-3 w-8/12">
+            <h2 class="font-bold">${item.info}</h2>
+            <p>${item.discription}</p>
+            <p class="text-[10px]">${item.avtor}</p>
+        </div>
+    </div>`
     })
 }
 
